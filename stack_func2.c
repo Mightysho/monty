e5d3832ec84f142893fa20b4f267c1a1cfc367da
Stacey-bee <78845157+Stacey-bee@users.noreply.github.com>
2021-09-24 02:07:07 +0300

    monty_stacks, queues; LIFO FIFO tasks

--- stack_func2.c
+++ stack_func2.c
@@ -77,7 +77,7 @@ void sub_nodes(stack_t **stack, unsigned int line_number)
 
 /**
  * div_nodes - Adds the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
  * @line_number: Interger representing the line number of of the opcode.
  */
 void div_nodes(stack_t **stack, unsigned int line_number)
